f1a56d7605e08ae48248b1894cefb67962a4e21f
Yazan <abo.ayash.yazan@gmail.com>
2025-04-20 14:26:03 +0200

    fixed depend for build

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, 'favicon.ico'), // path to your favicon
+    icon: path.resolve(__dirname, 'favicon.ico'), // path to your favicon
     webPreferences: {
       contextIsolation: true,
     },
